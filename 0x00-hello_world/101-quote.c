51bc2f7708751ef33c2662dbcdeecc02b57fc2d1
mohamadm890 <mohamad890098@gmail.com>
2023-09-07 10:58:36 -0700

    push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - 6-size.c
- * description - Write a C program that prints the size of various types on
- * the computer it is compiled and run on.
+ * main - 101-qoute.c
+ * description - Write a C program that prints exactly and that piece of
+ * art is useful\"
+ *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  *
  * Return: Always 0 (Success)
  */
-int main(void){
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+int main(void)
+{
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
 return (1);
 }
+
