028aabc246fab58200d2604b97bfc59a68e96e56
mohamadm890 <mohamad890098@gmail.com>
2023-09-07 05:57:54 -0700

    push

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - 6-size.c
+ * description - Write a C program that prints the size of various types on
+ * the computer it is compiled and run on.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+return (1);
+}
