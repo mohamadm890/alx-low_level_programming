7a6ee08ec49ee99bd904a5f85a27126f98b25da8
mohamadm890 <mohamad890098@gmail.com>
2023-09-07 05:06:10 -0700

    push

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - 5-printf.c
+ * description - Write a C program that prints exactly
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
