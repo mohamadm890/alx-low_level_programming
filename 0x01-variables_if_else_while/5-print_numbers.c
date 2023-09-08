bb79a90918e529be142c8b59154fde3664125d3e
mohamadm890 <mohamad890098@gmail.com>
2023-09-08 12:12:51 -0700

    add

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int letter;
+for (letter = 0; letter <= 9; letter++)
+{
+printf("%d ", letter);
+}
+printf("\n");
+return (0);
+}
