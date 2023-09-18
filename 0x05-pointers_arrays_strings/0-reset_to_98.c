26f550937eb7b0a0eaa6321f929b5a9029441e4e
mohamadm890 <mohamad890098@gmail.com>
2023-09-18 02:18:52 -0700

    w

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * reset_to_98 - a function used n as pointer to value in mermory.
+ * and changes the value it points to to 98
+ * @n: input
+ * Return: n
+ */
 void reset_to_98(int *n)
 {
 *n = 98;
