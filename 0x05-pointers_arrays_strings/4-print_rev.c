39547f3f31bf0f96ba9bbaf70bc268b8ed776357
mohamadm890 <mohamad890098@gmail.com>
2023-09-18 06:38:52 -0700

    e

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,14 @@
 void print_rev(char *s)
 {
 int size = 0;
+int i;
 while (*s != '\0')
 {
 size++;
 s++;
 }
 s--;
-for (int i = size - 1; i >= 0; i--)
+for (i = size - 1; i >= 0; i--)
 {
 _putchar(s[i]);
 }
