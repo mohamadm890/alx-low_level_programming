04590abc6f91590f69b5106ed51bdcd2bf179370
mohamadm890 <mohamad890098@gmail.com>
2023-09-21 01:58:23 -0700

    w

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, const char *src, int n)
+{
+if (dest == 'NULL' || src == 'NULL' || n == 0)
+{
+return (dest);
+}
+int i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+}
