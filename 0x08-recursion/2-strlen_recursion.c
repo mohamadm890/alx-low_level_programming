3f9ae35ba9a767eb1f38022af0fb5f8b3d9f42c2
mohamadm890 <mohamad890098@gmail.com>
2023-09-28 03:17:19 -0700

    w

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,8 +7,11 @@
  */
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-{return 0}
-else
-{return 1 + _strlen_recursion(s + 1)}
+int len = 0;
+if (*s)
+{
+len++;
+len += _strlen_recursion(s + 1);
+}
+return (len);
 }
