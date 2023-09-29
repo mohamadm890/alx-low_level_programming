28522deaecef884d56559a2a93415ab8432d88ef
mohamadm890 <mohamad890098@gmail.com>
2023-09-29 02:28:41 -0700

    e

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,9 +24,9 @@ return (actual_sqrt_recursion(n, 0));
  */
 int actual_sqrt_recursion(int n, int i)
 {
-if (i * i > n) 
+if (i * i > n)
 return (-1);
-if (i * i == n) 
+if (i * i == n)
 return (i);
 return (actual_sqrt_recursion(n, i + 1));
 }
