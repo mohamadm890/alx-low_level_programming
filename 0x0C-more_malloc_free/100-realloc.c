02cb84a7764e1148b118fb851d64decdbc072ec3
mohamadm890 <mohamad890098@gmail.com>
2023-10-08 11:39:00 -0700

    g

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *p1;
+char *ptr1;
 char *old_ptr;
 unsigned int i;
 if (new_size == old_size)
@@ -22,20 +22,20 @@ return (NULL);
 }
 if (!ptr)
 return (malloc(new_size));
-p1 = malloc(new_size);
-if (!p1)
+ptr1 = malloc(new_size);
+if (!ptr1)
 return (NULL);
 old_ptr = ptr;
 if (new_size < old_size)
 {
 for (i = 0; i < new_size; i++)
-p1[i] = old_ptr[i];
+ptr1[i] = old_ptr[i];
 }
 if (new_size > old_size)
-}
+{
 for (i = 0; i < old_size; i++)
-p1[i] = old_ptr[i];
+ptr1[i] = old_ptr[i];
 }
 free(ptr);
-return (p1);
+return (ptr1);
 }
