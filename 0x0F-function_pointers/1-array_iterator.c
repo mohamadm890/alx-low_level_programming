e5990d40809a80ec13a5085eee8fc395ce3db2ce
mohamadm890 <mohamad890098@gmail.com>
2023-10-11 02:36:39 -0700

    r

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-if (array == NULL || action == NULL) return;
+if (array == NULL || action == NULL)
+return;
 for (int i = 0; i < size; i++)
 {
-action(array[i]);  
+action(array[i]);
 }
 }
