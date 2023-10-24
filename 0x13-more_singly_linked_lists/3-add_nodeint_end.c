9e7bd85b96b8bbc6d244fc66f93b16b11cf977af
mohamadm890 <mohamad890098@gmail.com>
2023-10-24 01:26:46 -0700

    d

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,9 +8,9 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listin_t *node;
-listin_t *p = *head;
-node = malloc(sizeof(listin_t));
+listint_t *node;
+listint_t *p = *head;
+node = malloc(sizeof(listint_t));
 if (!node)
 {
 return (NULL);
